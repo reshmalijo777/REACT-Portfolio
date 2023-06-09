1a2ff54d36c4efe43c74321aed6a4f80c7149122
jesus love <snowley777@gmail.com>
2023-06-09 18:50:17 -0500

    added header component to my app

---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import React from 'React'
+import Navbar from '../Navigation';
+// Import React Router Link component for internal hyperlinks
+import {Link} from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div className='d-flex flex-row  h-100 justify-content-between'>
+         {/* Use Link component to create a link that returns to the homepage on click */}
+        <Link className='custom-header' to='/'>
+          <h1 className="m-0" style={{ fontSize: '3rem' }}> Reshma Paul </h1>
+        </Link>
+        <Navbar />
+        </div>
+    )
+};
+export default Header;
