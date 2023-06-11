4fe94699000c6c4caa93409db00683aca0ee9ddd
jesus love <snowley777@gmail.com>
2023-06-11 11:17:17 -0500

    added icon links to my  GitHub and LinkedIn profiles, and facebook page

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,8 +5,13 @@ function Footer(){
     return(
         <footer className= "footer d-flex">
             <div>
-                <a href ="https://github.com/reshmalijo777"><i className="fa-brands fa-github fa-xl px-3"></i></a>
-                <a href ="https://www.linkedin.com/in/reshma-paul-84bb53104/"><i className="fa-brands fa-linkedin fa-xl"></i></a>
+                <a href ="https://github.com/reshmalijo777" target="_blank" rel="noreferrer">
+                  <img alt="Github" className="footer-img " src={require("../../assets/icons8-github-24.png")} /></a>
+                <a href ="https://www.linkedin.com/in/reshma-paul-84bb53104/" target="_blank" rel="noreferrer">
+                  <img alt="Linkedin" className="footer-img" src={require("../../assets/icons8-linkedin-30.png")}/></a>
+                <a href ="https://www.facebook.com/reshma.paul.12" target="_blank" rel="noreferrer">
+                  <img alt="Facebook" className="footer-img" src={require("../../assets/icons8-facebook-30.png")}/></a>
+                
             </div>
 
             <div className="footer-copy">
